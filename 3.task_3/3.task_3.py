330daebe73c520930054b9a5f921537ce12bc5d4
Aydario <aydar.zh@mail.ru>
2024-09-15 01:20:29 +0300

    README created and task_3 updated

--- 3.task_3/3.task_3.py
+++ 3.task_3/3.task_3.py
@@ -1,20 +1,12 @@
-def work_with_files(f1, f2, f3, resulting_file):
-    with open(f1, 'r', encoding='utf8') as file_1, \
-            open(f2, 'r', encoding='utf8') as file_2, \
-            open(f3, 'r', encoding='utf8') as file_3:
-        dict_files = {
-            f1: file_1.readlines(),
-            f2: file_2.readlines(),
-            f3: file_3.readlines()
-        }
-    with open(resulting_file, 'w', encoding='utf-8') as result:
-        for strings in sorted(dict_files.items(), key=lambda x: x[-1], reverse=True):
-            name_file, list_strings = strings
-            result.write(name_file.split('\\')[-1] + '\n')
-            result.write(f'{len(list_strings)}\n')
-            result.writelines(list_strings)
-            result.write('\n')
-
+def work_with_files(*file_names):
+    sorted_file_data = sorted(((name, len(open(name, encoding='utf-8').readlines())) for name in file_names), key=lambda x: x[-1])
+    with open('3.task_3\\result.txt', 'w', encoding='utf-8') as result_file:
+        for name, length in sorted_file_data:
+            result_file.write(name.split("\\")[-1] + '\n')
+            result_file.write(f'{length}\n')
+            result_file.write(f'{open(name, encoding="utf-8").read()}\n')
+            
 
 work_with_files('3.task_3\one.txt', '3.task_3\\two.txt', 
-                '3.task_3\\three.txt', '3.task_3\\result.txt')
+                '3.task_3\\three.txt')
+#, 
